d47759aee6f56f917dad91e330c38011729e3705
jwong1209 <jwong1209qaz@gmail.com>
2022-08-26 00:45:42 -0700

    Added change text color button

--- app/src/main/java/com/example/helloworld/MainActivity.kt
+++ app/src/main/java/com/example/helloworld/MainActivity.kt
@@ -1,19 +1,31 @@
 package com.example.helloworld
 
+import android.graphics.Color
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import android.widget.Toast
+import org.w3c.dom.Text
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         val button = findViewById<Button>(R.id.helloButton)
+        val colorButton = findViewById<Button>(R.id.colorButton)
+        val introText = findViewById<TextView>(R.id.introTextView)
+
         button.setOnClickListener {
             Log.v("Helloworld", "Button clicked!")
             Toast.makeText(this, "Hello to you too", Toast.LENGTH_SHORT).show()
         }
+
+        colorButton.setOnClickListener {
+            Toast.makeText(this, "Color Changed", Toast.LENGTH_SHORT).show()
+            introText.setTextColor(Color.parseColor("magenta"))
+        }
+
     }
 }
